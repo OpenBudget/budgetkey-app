66ab98d84e3d6cccf788a2680c02e08fc681c01e
Adam Kariv <adam.kariv@gmail.com>
2023-10-08 23:10:37 +0300

    Fix budget item error

--- projects/budgetkey/src/app/item/items/item-budget/base-budget-item/base-budget-item.component.ts
+++ projects/budgetkey/src/app/item/items/item-budget/base-budget-item/base-budget-item.component.ts
@@ -23,7 +23,7 @@ export class BudgetItemComponent implements OnChanges{
     this.takana = this.item?.code?.length === 10; 
     if (this.item?.history) {
       for (let year = this.item.year-1; year > this.item.year - 5; year--) {
-        if (this.item.history[year].net_executed) {
+        if (this.item.history[year] && this.item.history[year].net_executed) {
           this.executedYear = year;
           break;
         }
