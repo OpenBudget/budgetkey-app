1d977ada7e012dce67fd0dbfbce33933d35a8d11
Adam Kariv <adam.kariv@gmail.com>
2023-10-08 23:13:50 +0300

    Add missing relative time from dayjs

--- projects/budgetkey/src/app/item/item.module.ts
+++ projects/budgetkey/src/app/item/item.module.ts
@@ -60,7 +60,11 @@ import { ItemSupportsComponent } from './items/item-supports/item-supports.compo
 import { SearchLinkComponent } from './search-link/search-link.component';
 import { NgxSeoModule } from '@avivharuzi/ngx-seo';
 
-
+import * as dayjs from 'dayjs';
+import 'dayjs/locale/he';
+import 'dayjs/plugin/duration';
+import 'dayjs/plugin/relativeTime';
+dayjs.locale('he')
 
 @NgModule({
   declarations: [
