d258b62e11d6465db0d6a2cc0dc9abfb9a72850b
Adam Kariv <adam.kariv@gmail.com>
2023-10-09 10:56:25 +0300

    Implement redirects in item page

--- projects/budgetkey/src/app/app-error-handler.ts
+++ projects/budgetkey/src/app/app-error-handler.ts
@@ -1,11 +1,13 @@
-import {ErrorHandler, Inject, Injectable, PLATFORM_ID} from '@angular/core';
+import {ErrorHandler, Inject, Injectable, Optional, PLATFORM_ID} from '@angular/core';
 import {isPlatformBrowser} from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { REQUEST } from '@nguniversal/express-engine/tokens';
+import { Request } from 'express';
 
 @Injectable()
 export class AppErrorHandler implements ErrorHandler {
 
-  constructor(@Inject(PLATFORM_ID) private platformId: any, private route: ActivatedRoute) {
+  constructor(@Inject(PLATFORM_ID) private platformId: any, private route: ActivatedRoute, @Optional() @Inject(REQUEST) private request: Request) {
   }
 
   handleError(error: any): void {
@@ -17,10 +19,11 @@ export class AppErrorHandler implements ErrorHandler {
   }
   
   handleBrowser(error: any): void {
-    console.log('ERR', new Date(), error);
+    console.log('ERR', new Date().toISOString(), error.message || error);
   }
   
   handleServer(error: any): void {
-    console.log('ERR', new Date(), this.route.snapshot.url, error);
+    const path = this.request?.originalUrl || this.route.snapshot.url;
+    console.log('ERR', new Date().toISOString(), path, error.message, '\n' + error.stack);
   }
 }
--- projects/budgetkey/src/app/item/item-api.service.ts
+++ projects/budgetkey/src/app/item/item-api.service.ts
@@ -1,18 +1,33 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Subject, map } from 'rxjs';
+import { Inject, Injectable, Optional } from '@angular/core';
+import { Subject, map, tap } from 'rxjs';
 import { GlobalSettingsService } from '../common-components/global-settings.service';
+import { Router } from '@angular/router';
+import { PlatformService } from '../common-components/platform.service';
+import { REQUEST } from '@nguniversal/express-engine/tokens';
+import { Request } from 'express';
 
 @Injectable()
 export class ItemApiService {
 
   questions = new Subject<any[] | null>()
 
-  constructor(private http: HttpClient, private globalSettings: GlobalSettingsService) {}
+  constructor(private http: HttpClient, private globalSettings: GlobalSettingsService, private router: Router, private ps: PlatformService, @Optional() @Inject(REQUEST) private request: Request) {}
 
   fetchItem(id: string) {
     return this.http.get('https://next.obudget.org/get/' + id).pipe(
-      map((res: any) => res.value)
+      map((res: any) => res.value),
+      tap((item: any) => {
+        if (item.__redirect) {
+          const to = `/i/${item.__redirect}`;
+          this.ps.browser(() => {
+            this.router.navigateByUrl(to);
+          });
+          this.ps.server(() => {
+            this.request?.res?.redirect(to);
+          });
+        }
+      })
     );
   }
 
