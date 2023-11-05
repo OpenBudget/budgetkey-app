0b64b49525e9dc36c2b5041fcb932cfca861ef57
Adam Kariv <adam.kariv@gmail.com>
2023-11-05 12:46:26 +0200

    dev

---
+++ dev-deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+git checkout dev && \
+(git branch -D dist || true) && \
+git checkout -b dist && \
+rm .gitignore && \
+cd utils && ./mk_bubbles.sh && cd .. && \
+npm run build && \
+cp dist/budgetkey/index.html dist/budgetkey/404.html && \
+cp CNAME dist/budgetkey/ || true && \
+git add dist/budgetkey && \
+git commit -m dist && \
+(git branch -D gh-pages || true) && \
+git subtree split --prefix dist/budgetkey -b gh-pages && \
+git push -f origin gh-pages:gh-pages && \
+git checkout dev && \
+git branch -D gh-pages && \
+git branch -D dist && \
+git checkout . 
